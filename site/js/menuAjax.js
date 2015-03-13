df71434401ada9adddeb4ac8067c11cb0d0a4872
Jake Ovenden <jake.dale@xtra.co.nz>
2015-03-13 15:57:16 +1300

    tweaked menuAjax to include map functionality

--- site/js/menuAjax.js
+++ site/js/menuAjax.js
@@ -10,15 +10,18 @@ $(document).ready(function(){
 			dataType: "json",
 			url: "ajax/"+menuType+".json",
 			success: function(data){
-				if(menuType!='x'){
-					$('#menu').append('<h2>'+menuType+'</h2>');
-					$(data).each(function(i){
-						$('#menu').append('<h3>'+data[i].name+'</h3><p>'+data[i].description+'</p><p><em>'+data[i].price+'</em></p>');
-					})
-					$('#menu').append('<button value="x" id="exit">x</button>');
-				} else {
+				if(menuType == 'Desserts' || menuType == 'Breakfast' || menuType == 'Starters' || menuType == 'Mains' || menuType == 'Drinks' ){
+					if(menuType!='x'){
+						$('#menu').append('<h2>'+menuType+'</h2>');
+						$(data).each(function(i){
+							$('#menu').append('<h3>'+data[i].name+'</h3><p>'+data[i].description+'</p><p><em>'+data[i].price+'</em></p>');
+						})
+						$('#menu').append('<button value="x" id="exit">x</button>');
+					} else {
 					$('#menu').html('');
+					}
 				}
+			
 			}
 		});
 
